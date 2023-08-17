efa24f443dfdeb1f5f111ee06aa9969ecbeed0f5
Zoldok <max_kyrtimov@mail.ru>
2023-08-17 17:22:27 +0300

    hw-1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 className="App-name">Здесь скоро будет Skypro.Music</h1>
+        <h1 className="App-name">Здесь скоро будет Skypro.Music</h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
