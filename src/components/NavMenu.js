48944de006603dfbf334739b800791028b44c0f9
Zoldok <max_kyrtimov@mail.ru>
2023-08-21 22:55:17 +0300

    рабочий бургер

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,17 +1,26 @@
 import '../App.css'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
 
 function NavMenu() {
+  const [isMenuVisible, setMenuVisible] = useState(false)
+
+  const toggleMenu = () => {
+    console.log('Toggle menu clicked')
+    setMenuVisible(!isMenuVisible)
+  }
+
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
+      <div className="nav__burger burger" onClick={toggleMenu}>
         <span className="burger__line" />
         <span className="burger__line" />
         <span className="burger__line" />
       </div>
-      <div className="nav__menu menu">
+      <div className={`nav__menu menu ${isMenuVisible ? 'visible' : 'hidden'}`}>
         <ul className="menu__list">
           <li className="menu__item">
             <a href="#" className="menu__link">
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,46 +6,11 @@ import {SidebarPlaylistDay, SidebarPlaylistIndy, SidebarPlaylist100} from './Sid
 function Sidebar() {
   return (
     <div className="main__sidebar sidebar">
-      {/* <div className="sidebar__personal">
-        <p className="sidebar__personal-name">Sergey.Ivanov</p>
-        <div className="sidebar__icon">
-          <svg alt="logout">
-            <use xlinkHref="img/icon/sprite.svg#logout" />
-          </svg>
-        </div>
-      </div> */}
       <SidebarLogin />
       <div className="sidebar__block">
         <div className="sidebar__list">
-          {/* <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist01.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div> */}
           <SidebarPlaylistDay />
-          {/* <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist02.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div> */}
           <SidebarPlaylist100 />
-          {/* <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist03.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div> */}
           <SidebarPlaylistIndy />
         </div>
       </div>
