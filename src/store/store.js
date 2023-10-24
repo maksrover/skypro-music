b09cab89de9b16018ecb710aba3b275bb337ddc2
maksrover <maksrover@gmail.com>
2023-10-24 15:07:55 +0300

    skypro-music created

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import audioPlayerSlice from '../store/useAudioPlayer/AudioPlayer.slise';
-
-export const store = configureStore({
-  reducer: {
-    audioPlayer: audioPlayerSlice,
-  },
-  devTools: true,
-});
