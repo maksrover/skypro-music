4086536683e09af20e1a1c45a6fe99b7472688c3
maksrover <maksrover@gmail.com>
2023-10-24 15:46:45 +0300

    test file deleted

--- skypro-music-app/src/App.js
+++ skypro-music-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className='App-text'>
           Здесь скоро будет Skypro.Music
         </p>
       </header>
