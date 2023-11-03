6a17c582351e4ce6c6d0f822fbc481c39cdc43cc
maksrover <maksrover@gmail.com>
2023-11-03 22:56:26 +0300

    Skeleton testing

--- skypro-music-app/src/App.jsx
+++ skypro-music-app/src/App.jsx
@@ -1,29 +1,28 @@
 import React from "react";
-import "./App.css";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import NavBar from "./components/NavBar.jsx";
 import CenterBlock from "./components/CenterBlock";
 import Sidebar from "./components/Sidebar";
 import PlayerBar from "./components/PlayerBar";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <div className="container">
-          <main className="main">
-            <NavBar />
-
-            <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+        <div className="wrapper">
+          <div className="container">
+            <main className="main">
+              <Skeleton count={50} />
+              <NavBar />
               <CenterBlock />
-              <Skeleton count={30} />
-            </SkeletonTheme>
-            <Sidebar />
-          </main>
-          <PlayerBar />
+              <Sidebar />
+            </main>
+            <PlayerBar />
+          </div>
         </div>
-      </div>
+      </SkeletonTheme>
       <Footer />
     </div>
   );
