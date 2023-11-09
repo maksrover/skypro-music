e51c32d91a3982f13652c6aad6700c6ff173ac06
maksrover <maksrover@gmail.com>
2023-11-09 21:03:58 +0300

    PlayerBar.styled

---
+++ skypro-music-app/src/components/PlayerBar/PlayerBar.Styled.js
@@ -0,0 +1,284 @@
+import { styled } from "styled-components";
+
+export const Bar = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  padding: 0 27px 0 31px;
+`;
+
+export const BarContent = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+`;
+
+export const PlayerProgress = styled.div`
+  /* Styles from playerBar.css */
+`;
+
+export const PlayerBlock = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+`;
+
+export const Player = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+`;
+
+export const PlayerControls = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  padding: 0 27px 0 31px;
+`;
+
+export const PlayerBtnPrev = styled.div`
+  padding: 5px;
+  margin-right: 23px;
+`;
+
+export const PlayerBtnPrevSvg = styled.svg`
+  width: 15px;
+  height: 14px;
+`;
+
+export const PlayerBtnPlay = styled.div`
+  padding: 5px;
+  margin-right: 23px;
+`;
+
+export const PlayerBtnPlaySvg = styled.svg`
+  width: 22px;
+  height: 20px;
+  fill: #d9d9d9;
+`;
+
+export const PlayerBtnNext = styled.div`
+  padding: 5px;
+  margin-right: 28px;
+  fill: #a53939;
+`;
+
+export const PlayerBtnNextSvg = styled.svg`
+  width: 15px;
+  height: 14px;
+  fill: inherit;
+  stroke: #d9d9d9;
+`;
+
+export const PlayerBtnRepeat = styled.div`
+  padding: 5px;
+  margin-right: 24px;
+`;
+
+export const PlayerBtnRepeatSvg = styled.svg`
+  width: 18px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`;
+
+export const PlayerBtnShuffle = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`;
+
+export const PlayerBtnShuffleSvg = styled.svg`
+  width: 19px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`;
+
+export const TrackPlay = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+`;
+
+export const TrackPlayContain = styled.div`
+  width: auto;
+  display: -ms-grid;
+  display: grid;
+  -ms-grid-columns: auto 1fr;
+  grid-template-columns: auto 1fr;
+  grid-template-areas: "image author" "image album";
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`;
+
+export const TrackPlayImage = styled.div`
+  width: 51px;
+  height: 51px;
+  background-color: #313131;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin-right: 12px;
+  -ms-grid-row: 1;
+  -ms-grid-row-span: 2;
+  -ms-grid-column: 1;
+  grid-area: image;
+`;
+
+export const TrackPlaySvg = styled.svg`
+  width: 18px;
+  height: 17px;
+  fill: transparent;
+  stroke: #4e4e4e;
+`;
+
+export const TrackPlayAuthor = styled.div`
+  -ms-grid-row: 1;
+  -ms-grid-column: 2;
+  grid-area: author;
+  min-width: 49px;
+`;
+
+export const TrackPlayAuthorLink = styled.a`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  white-space: nowrap;
+`;
+
+export const TrackPlayAlbum = styled.div`
+  -ms-grid-row: 2;
+  -ms-grid-column: 2;
+  grid-area: album;
+  min-width: 49px;
+`;
+
+export const TrackPlayAlbumLink = styled.a`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 24px;
+  color: #ffffff;
+`;
+
+export const TrackPlayLikeDis = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-left: 26%;
+`;
+
+export const TrackPlayLike = styled.div`
+  padding: 5px;
+`;
+
+export const TrackPlayLikeSvg = styled.svg`
+  width: 14px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`;
+
+export const TrackPlayDislike = styled.div`
+  margin-left: 28.5px;
+`;
+
+export const TrackPlayDislikeSvg = styled.svg`
+  width: 14.34px;
+  height: 13px;
+  fill: transparent;
+  stroke: #696969;
+`;
+
+export const VolumeBlock = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: end;
+  -ms-flex-pack: end;
+  justify-content: end;
+`;
+
+export const VolumeContent = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`;
+
+export const VolumeImage = styled.div`
+  width: 13px;
+  height: 18px;
+  margin-right: 17px;
+`;
+
+export const VolumeSvg = styled.svg`
+  width: 13px;
+  height: 18px;
+  fill: transparent;
+`;
+
+export const VolumeProgress = styled.div`
+  width: 109px;
+`;
+
+export const VolumeProgressLine = styled.input`
+  width: 109px;
+`;
---
+++ skypro-music-app/src/components/PlayerBar/PlayerBar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import * as S from "./PlayerBar.Styled";
+
+function PlayerBar() {
+  return (
+    <S.Bar>
+      <S.BarContent>
+        <S.PlayerProgress></S.PlayerProgress>
+        <S.PlayerBlock>
+          <S.Player>
+            <S.PlayerControls>
+              <S.PlayerBtnPrev>
+                <S.PlayerBtnPrevSvg alt="prev">
+                  <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
+                </S.PlayerBtnPrevSvg>
+              </S.PlayerBtnPrev>
+              <S.PlayerBtnPlay>
+                <S.PlayerBtnPlaySvg alt="play">
+                  <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+                </S.PlayerBtnPlaySvg>
+              </S.PlayerBtnPlay>
+              <S.PlayerBtnNext>
+                <S.PlayerBtnNextSvg alt="next">
+                  <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
+                </S.PlayerBtnNextSvg>
+              </S.PlayerBtnNext>
+              <S.PlayerBtnRepeat>
+                <S.PlayerBtnRepeatSvg alt="repeat">
+                  <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+                </S.PlayerBtnRepeatSvg>
+              </S.PlayerBtnRepeat>
+              <S.PlayerBtnShuffle>
+                <S.PlayerBtnShuffleSvg alt="shuffle">
+                  <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
+                </S.PlayerBtnShuffleSvg>
+              </S.PlayerBtnShuffle>
+            </S.PlayerControls>
+            <S.TrackPlay>
+              <S.TrackPlayContain>
+                <S.TrackPlayImage>
+                  <S.TrackPlaySvg alt="music">
+                    <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                  </S.TrackPlaySvg>
+                </S.TrackPlayImage>
+                <S.TrackPlayAuthor>
+                  <S.TrackPlayAuthorLink href="http://">
+                    Ты та...
+                  </S.TrackPlayAuthorLink>
+                </S.TrackPlayAuthor>
+                <S.TrackPlayAlbum>
+                  <S.TrackPlayAlbumLink href="http://">
+                    Баста
+                  </S.TrackPlayAlbumLink>
+                </S.TrackPlayAlbum>
+              </S.TrackPlayContain>
+              <S.TrackPlayLikeDis>
+                <S.TrackPlayLike>
+                  <S.TrackPlayLikeSvg alt="like">
+                    <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                  </S.TrackPlayLikeSvg>
+                </S.TrackPlayLike>
+                <S.TrackPlayDislike>
+                  <S.TrackPlayDislikeSvg alt="dislike">
+                    <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
+                  </S.TrackPlayDislikeSvg>
+                </S.TrackPlayDislike>
+              </S.TrackPlayLikeDis>
+            </S.TrackPlay>
+          </S.Player>
+          <S.VolumeBlock>
+            <S.VolumeContent>
+              <S.VolumeImage>
+                <S.VolumeSvg alt="volume">
+                  <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
+                </S.VolumeSvg>
+              </S.VolumeImage>
+              <S.VolumeProgress>
+                <S.VolumeProgressLine type="range" name="range" />
+              </S.VolumeProgress>
+            </S.VolumeContent>
+          </S.VolumeBlock>
+        </S.PlayerBlock>
+      </S.BarContent>
+    </S.Bar>
+  );
+}
+
+export default PlayerBar;
