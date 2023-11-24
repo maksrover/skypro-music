7803aa0a68af4c70fa8abe3189d8873a58393a05
maksrover <maksrover@gmail.com>
2023-11-24 21:06:55 +0300

    login

--- skypro-music-app/src/App.js
+++ skypro-music-app/src/App.js
@@ -1,21 +1,12 @@
 import { GlobalStyle } from "./pages/mainFolder/globalStyle";
-import { Main } from "./pages/mainFolder/main";
-import { useState } from "react";
+
 import { AppRoutes } from "./routes";
 
 export function App() {
-  const initialUserState = localStorage.getItem("user") === "true";
-  const [isLoggedIn, setIsLoggedIn] = useState(initialUserState);
-  const handleLogin = () => {
-    localStorage.setItem("isLoggedIn", "setIsLoggedIn");
-    setIsLoggedIn(true);
-  };
-
   return (
     <>
       <GlobalStyle />
-      <AppRoutes isLoggedIn={isLoggedIn} onAuthButtonClick={handleLogin} />
-      <Main />
+      <AppRoutes />
     </>
   );
 }
--- skypro-music-app/src/pages/login/loginUser.jsx
+++ skypro-music-app/src/pages/login/loginUser.jsx
@@ -1,5 +1,33 @@
 import * as S from "../mainFolder/main.styled";
+import * as L from "./login.styled";
+import { useNavigate } from "react-router-dom";
 
-export const Login = () => {
-  return <S.Wrapper></S.Wrapper>;
+export const Login = ({ onAuthButtonClick }) => {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <L.CenterBlock>
+          <L.CenterBlockItem>
+            <L.LogoImage>
+              <L.LogoImageItem src="/img/logoBlack.png" alt="logo" />
+            </L.LogoImage>
+            <L.InputBlock>
+              <L.InputItem type="username" placeholder="Почта" />
+
+              <L.InputItem type="password" placeholder="Пароль" />
+            </L.InputBlock>
+            <L.ButtonBlock>
+              <L.ButtonEnter to="/" onClick={onAuthButtonClick}>
+                Войти
+              </L.ButtonEnter>
+
+              <L.ButtonRegister to="/register">
+                Зарегистрироваться
+              </L.ButtonRegister>
+            </L.ButtonBlock>
+          </L.CenterBlockItem>
+        </L.CenterBlock>
+      </S.Container>
+    </S.Wrapper>
+  );
 };
