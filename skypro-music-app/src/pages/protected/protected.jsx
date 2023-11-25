4dc292bdfe985fcbe20a03c62bfa23a976168c52
maksrover <maksrover@gmail.com>
2023-11-25 18:08:20 +0300

    protected

--- skypro-music-app/src/pages/protected/protected.jsx
+++ skypro-music-app/src/pages/protected/protected.jsx
@@ -1,7 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-export const ProtectedRoute = ({ redirectPath = "/login", isAllowed }) => {
-  if (!isAllowed) {
+export const ProtectedRoute = ({ redirectPath = "/login" }) => {
+  const [cookies] = useCookies(["token"]);
+  const hasToken = !!cookies.token;
+
+  if (!hasToken) {
     return <Navigate to={redirectPath} replace={true} />;
   }
 
--- skypro-music-app/src/routes.jsx
+++ skypro-music-app/src/routes.jsx
@@ -6,19 +6,28 @@ import { MyTracks } from "./pages/myTracks/myTracks";
 import { PlaylistOfTheDay } from "./pages/compilations/playlistOfTheDay";
 import { DanceHits } from "./pages/compilations/DanceHits";
 import { IndieCharge } from "./pages/compilations/indieCharge";
+import { ProtectedRoute } from "./pages/protected/protected";
 import { NotFound } from "./pages/404/NotFound";
 
-export const AppRoutes = () => {
+export const AppRoutes = ({ isLoggedIn, onAuthButtonClick }) => {
   return (
     <Routes>
-      <Route path="/favorites" element={<MyTracks />} />
-      <Route path="/category/1" element={<PlaylistOfTheDay />} />
-      <Route path="/" element={<Main />} />
-      <Route path="/category/2" element={<DanceHits />} />
-      <Route path="//category/3" element={<IndieCharge />} />
+      <Route element={<ProtectedRoute isAllowed={isLoggedIn} />}>
+        <Route path="/favorites" element={<MyTracks />} />
+        <Route path="/category/1" element={<PlaylistOfTheDay />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/category/2" element={<DanceHits />} />
+        <Route path="//category/3" element={<IndieCharge />} />
+      </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<RegisterUser />} />
+      <Route
+        path="/login"
+        element={<Login onAuthButtonClick={onAuthButtonClick} />}
+      />
+      <Route
+        path="/register"
+        element={<RegisterUser onAuthButtonClick={onAuthButtonClick} />}
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
