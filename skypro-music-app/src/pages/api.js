985093be387e6f567b404be63fcae292b28508a8
maksrover <maksrover@gmail.com>
2023-12-13 18:49:03 +0300

    api corrected

--- skypro-music-app/src/pages/api.js
+++ skypro-music-app/src/pages/api.js
@@ -1,11 +1,12 @@
 export async function getTrack() {
-  const trackResponse = (
-    await fetch(` 	https://skypro-music-api.skyeng.tech/catalog/track/all/`)
-  ).json();
+  const trackResponse = await fetch(
+    "https://skypro-music-api.skyeng.tech/catalog/track/all/"
+  );
+  const trackData = await trackResponse.json();
 
-  if (!trackResponse) {
-    throw new Error("Ошибка сервера");
+  if (!trackData) {
+    throw new Error("Failed to fetch tracks");
   }
 
-  return trackResponse;
+  return trackData;
 }
